97993f85ed3f2cfc0bbe27c1bcf9b46322d83282
ThracusRO <samuel.trif@gmail.com>
2024-11-16 11:14:30 +0200

    Add project files.

---
+++ Data/Trif_Samuel_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Trif_Samuel_Lab2.Models;
+
+namespace Trif_Samuel_Lab2.Data
+{
+    public class Trif_Samuel_Lab2Context : DbContext
+    {
+        public Trif_Samuel_Lab2Context (DbContextOptions<Trif_Samuel_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Trif_Samuel_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Trif_Samuel_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Trif_Samuel_Lab2.Data;
+using Trif_Samuel_Lab2.Models;
+
+namespace Trif_Samuel_Lab2.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Trif_Samuel_Lab2.Data.Trif_Samuel_Lab2Context _context;
+
+        public DetailsModel(Trif_Samuel_Lab2.Data.Trif_Samuel_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
