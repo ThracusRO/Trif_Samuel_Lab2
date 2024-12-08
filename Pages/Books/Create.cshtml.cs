74f156029fe56fea430cc8ad86381fcdc2b46e6a
ThracusRO <samuel.trif@gmail.com>
2024-12-08 22:14:01 +0100

    Final Laborator3 functional

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Trif_Samuel_Lab2.Models;
 
 namespace Trif_Samuel_Lab2.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Trif_Samuel_Lab2.Data.Trif_Samuel_Lab2Context _context;
 
@@ -27,10 +27,13 @@ namespace Trif_Samuel_Lab2.Pages.Books
             // "FirstName", "LastName");
             var authorList = _context.Author.Select(x => new
             {
-                x.ID,
-                FullName = x.FirstName + " " + x.LastName
+                   x.ID,
+                   FullName = x.FirstName + " " + x.LastName
             });
-            ViewData["AuthorID"] = new SelectList(authorList, "ID", "FullName");
+             ViewData["AuthorID"] = new SelectList(authorList, "ID", "FullName");
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
             return Page();
         }
 
@@ -38,16 +41,38 @@ namespace Trif_Samuel_Lab2.Pages.Books
         public Book Book { get; set; } = default!;
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
+        /* conform model Lab3 seciunea asta trebuia inlocuita
+
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+           if (!ModelState.IsValid)
+           {
+               return Page();
+           }
 
+           _context.Book.Add(Book);
+           await _context.SaveChangesAsync();
+
+           return RedirectToPage("./Index");
+        } */
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
+        {
+            var newBook = new Book();
+            if (selectedCategories != null)
+            {
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
+            }
+            Book.BookCategories = newBook.BookCategories;
             _context.Book.Add(Book);
             await _context.SaveChangesAsync();
-
             return RedirectToPage("./Index");
         }
     }
--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -11,7 +11,7 @@ using Trif_Samuel_Lab2.Models;
 
 namespace Trif_Samuel_Lab2.Pages.Books
 {
-    public class EditModel : PageModel
+    public class EditModel : BookCategoriesPageModel
     {
         private readonly Trif_Samuel_Lab2.Data.Trif_Samuel_Lab2Context _context;
 
@@ -30,12 +30,24 @@ namespace Trif_Samuel_Lab2.Pages.Books
                 return NotFound();
             }
 
-            var book =  await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
-            if (book == null)
+            Book = await _context.Book
+            .Include(b => b.Publisher)
+            .Include(b => b.BookCategories).ThenInclude(b => b.Category)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(m => m.ID == id);
+
+           // var book =  await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+                        
+            if (Book == null)
             {
                 return NotFound();
             }
-            Book = book;
+           
+            //apelam PopulateAssignedCategoryData pentru o obtine informatiile necesare checkbox-
+            //urilor folosind clasa AssignedCategoryData
+            PopulateAssignedCategoryData(_context, Book);
+
+           // Book = book;
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
 "PublisherName");
             var authorList = _context.Author.Select(x => new
@@ -51,6 +63,9 @@ namespace Trif_Samuel_Lab2.Pages.Books
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more information, see https://aka.ms/RazorPagesCRUD.
+
+
+        /* Partea initiala Lab2
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
@@ -77,11 +92,46 @@ namespace Trif_Samuel_Lab2.Pages.Books
             }
 
             return RedirectToPage("./Index");
-        }
+        } 
 
         private bool BookExists(int id)
         {
             return _context.Book.Any(e => e.ID == id);
+        }*/
+        public async Task<IActionResult> OnPostAsync(int? id, string[]
+        selectedCategories)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            //se va include Author conform cu sarcina de la lab 2
+            var bookToUpdate = await _context.Book
+            .Include(i => i.Publisher)
+            .Include(i => i.BookCategories)
+            .ThenInclude(i => i.Category)
+            .FirstOrDefaultAsync(s => s.ID == id);
+            if (bookToUpdate == null)
+            {
+                return NotFound();
+            }
+            //se va modifica AuthorID conform cu sarcina de la lab 2
+            if (await TryUpdateModelAsync<Book>(
+            bookToUpdate,
+            "Book",
+            i => i.Title, i => i.Author,
+            i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            //Apelam UpdateBookCategories pentru a aplica informatiile din checkboxuri la entitatea Books care
+            //este editata
+            UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+            PopulateAssignedCategoryData(_context, bookToUpdate);
+            return Page();
         }
+
     }
 }
