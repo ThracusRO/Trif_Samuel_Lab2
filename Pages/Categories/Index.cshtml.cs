ddba7fd39392e819496b9616a14c0fcc67839748
ThracusRO <samuel.trif@gmail.com>
2024-12-14 21:29:16 +0200

    Finalizare  Lab4

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Trif_Samuel_Lab2.Data;
 using Trif_Samuel_Lab2.Models;
+using Trif_Samuel_Lab2.Models.ViewModels;
 
 namespace Trif_Samuel_Lab2.Pages.Categories
 {
@@ -20,10 +21,36 @@ namespace Trif_Samuel_Lab2.Pages.Categories
         }
 
         public IList<Category> Category { get;set; } = default!;
+       
+        /*Task Lab4 sarcina laborator*/
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
 
+        public async Task OnGetAsync(int? id, int? bookID)
+        {
+            CategoryData = new CategoryIndexData();
+            
+            CategoryData.Categories = await _context.Category
+            .Include(c => c.BookCategories)
+            .ThenInclude(bc => bc.Book)
+            .ThenInclude(b => b.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+           
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(c => c.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories.Select(bc => bc.Book);
+            }
+        }
+        /* initial pre Lab4 sarcina laborator
         public async Task OnGetAsync()
         {
             Category = await _context.Category.ToListAsync();
         }
+        */
     }
 }
